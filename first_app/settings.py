9e69e9c0bd2c1072bdad6a1c59ce7b00af2d3ad0
holojva <holojva@gmail.com>
2022-03-25 18:34:52 +0700

    kostya look VNIMATELNO

--- first_app/settings.py
+++ first_app/settings.py
@@ -88,7 +88,7 @@ if not DEVELOPMENT:
     from .prod import DEBUG, ALLOWED_HOSTS, MIDDLEWARE
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update[db_from_env]
+    DATABASES['default'].update(db_from_env)
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 else:
     from .dev import DEBUG, ALLOWED_HOSTS, MIDDLEWARE
