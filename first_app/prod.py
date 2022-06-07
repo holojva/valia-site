9fc88377f684cf81e124804bfbfadc5c1494fac4
holojva <holojva@gmail.com>
2022-06-07 19:48:14 +0300

    make it better

--- first_app/prod.py
+++ first_app/prod.py
@@ -1,6 +1,6 @@
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://enigmatic-shelf-68472.herokuapp.com/', "127.0.0.1"]
+ALLOWED_HOSTS = ['enigmatic-shelf-68472.herokuapp.com', "127.0.0.1"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
